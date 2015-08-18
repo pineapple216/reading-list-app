1af5e243aac82e229e69edd9ca85ccb169dda8aa
Andrew Solomon <andrewsolomon.edu@gmail.com>
2015-08-18 14:53:06 -0400

    Added ArticleTableViewCell and UIWebViewController

--- Reading List App/Reading List App/HomeTableViewController.swift
+++ Reading List App/Reading List App/HomeTableViewController.swift
@@ -9,21 +9,48 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
+    
+    private let articles = Article.allArticles()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.estimatedRowHeight = 58
-//        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
-    
+   
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return articles.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("ArticleCell") as UITableViewCell!
+        let cell = tableView.dequeueReusableCellWithIdentifier("ArticleCell") as! ArticleTableViewCell
+        let article = articles[indexPath.row]
+        
+        cell.title = article.title
+        cell.url = article.url
+        cell.date = article.date
         
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let article = articles[indexPath.row]
+        performSegueWithIdentifier("WebSegue", sender: article)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "WebSegue" {
+            if let article = sender as? Article,
+                let vc = segue.destinationViewController as? WebViewController,
+                    let url = NSURL(string: article.url) {
+                    let request = NSURLRequest(URL: url)
+                    vc.loadRequest(request)
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 58
     }
 }
